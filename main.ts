5208aa14024da156d735bfc733465bbf320db9a7
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2023-01-05 16:32:26 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,41 @@
-let x = 0
 let b = 0
 let a = 0
 let sprite = game.createSprite(2, 2)
+let man = game.createSprite(randint(0, 4), randint(0, 4))
 basic.forever(function () {
     a = input.acceleration(Dimension.X)
     b = input.acceleration(Dimension.Y)
 })
 basic.forever(function () {
-    if (x > 4) {
-        x = 4
-    }
-    if (x < -4) {
-        x = -4
+    if (a > 0 && a <= 1023) {
+        sprite.move(1)
+        basic.pause(150)
     }
 })
 basic.forever(function () {
-    if (a > 0 && a <= 1023) {
-        basic.clearScreen()
-        sprite.move(1)
-        basic.pause(200)
-    }
     if (a < 0 && a >= -1023) {
-        basic.clearScreen()
         sprite.move(-1)
-        basic.pause(200)
+        basic.pause(150)
+    }
+})
+basic.forever(function () {
+    if (b < 0 && b >= -1023) {
+        sprite.change(LedSpriteProperty.Y, -1)
+        basic.pause(150)
     }
 })
 basic.forever(function () {
     if (b > 0 && b <= 1023) {
-        basic.clearScreen()
         sprite.change(LedSpriteProperty.Y, 1)
-        basic.pause(200)
+        basic.pause(150)
     }
-    if (b < 0 && b >= -1023) {
-        basic.clearScreen()
-        sprite.change(LedSpriteProperty.Y, -1)
-        basic.pause(200)
+})
+basic.forever(function () {
+    if (sprite) {
+        if (sprite.isTouching(man)) {
+            game.addScore(1)
+            man.delete()
+            man = game.createSprite(randint(0, 4), randint(0, 4))
+        }
     }
 })
